3d5eaec0a04a1722a60e2e9cae2e2c1c2fffead4
Camilo Rojas <camilo.rojas1@mail.udp.cl>
2024-07-08 23:56:16 -0400

    custom pool

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,29 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+const { cfnUserPool } = backend.auth.resources.cfnResources;
+// modify cfnUserPool policies directly
+cfnUserPool.policies = {
+  passwordPolicy: {
+    minimumLength: 10,
+    requireLowercase: true,
+    requireNumbers: true,
+    requireSymbols: true,
+    requireUppercase: true,
+    temporaryPasswordValidityDays: 20,
+  },
+};
+
+// update the schema property to add custom attributes
+if (Array.isArray(cfnUserPool.schema)) {
+  cfnUserPool.schema.push({
+    name: 'policyName',
+    attributeDataType: 'Boolean',
+    developerOnlyAttribute: true,
+  });
+}
