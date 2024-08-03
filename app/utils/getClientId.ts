fc0588da82d04cb91b3e721f890bd654a6929dda
Camilo Rojas <camilo.rojas1@mail.udp.cl>
2024-08-03 03:44:42 -0400

    fix

--- app/utils/getClientId.ts
+++ app/utils/getClientId.ts
@@ -10,7 +10,7 @@ export const getPoolId = async () => {
     switch (uri) {
         case "new-domain-test.vercel.app":
             return "25f5223fhkvjprn629msot83v9"
-        case "amplify-next-template-sage":
+        case "amplify-next-template-sage.vercel.app":
             return "iu56o6uhj7q895k38542vqacf"
         default:
             return "25f5223fhkvjprn629msot83v9"
